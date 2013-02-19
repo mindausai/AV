a5c5be652dfeb130f40d320af28a40734079d9f4
Marat Subkhankulov <skifmg@hotmail.com>
2013-02-19 22:45:30 +0000

    Added some helper functions

--- show_gt.m
+++ show_gt.m
@@ -1,15 +1,19 @@
 load gt1
 
 files = dir('images/0*.jpg');
+bg= imread('background.jpg');
 
 figure(1);
+current = imread(strcat('images/',files(1).name));
 for ii = 1:size(files,1)
-    Image = imread(strcat('images/',files(ii).name));
-    imshow(Image);
+    prev = current;
+    current = imread(strcat('images/',files(ii).name));
+    %imshow(Image);
     hold on;
-    plot(gt1(3,ii),gt1(2,ii),'r.');
-    plot(gt1(5,ii),gt1(4,ii),'g.');
-    plot(gt1(7,ii),gt1(6,ii),'y.');
+    %plot(gt1(3,ii),gt1(2,ii),'r.');
+    %plot(gt1(5,ii),gt1(4,ii),'g.');
+    %plot(gt1(7,ii),gt1(6,ii),'y.');
+    %dohist(Image, 1);
+    imshow(current-bg);
     drawnow;
-    pause(1)
 end
