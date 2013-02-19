4e2fd23fba821a6d55b63ca052b990953b0d7da4
mindausai <mindausai@gmail.com>
2013-02-19 21:30:21 +0000

    some changes in detection

--- test2.m
+++ test2.m
@@ -1,4 +1,4 @@
-image1 = double(imread('images/00000040.jpg','jpg'));
+image1 = double(imread('images/00000030.jpg','jpg'));
 bg = double(imread('background.jpg','jpg'));
 
 figure(3)
@@ -33,14 +33,39 @@ bg(:,:,2) = bg(:,:,2)./sumBg;
 bg(:,:,3) = bg(:,:,3)./sumBg;
 
 image1(:,:,2) = image1(:,:,2) - bg(:,:,2) > -0.07;
-image1(:,:,1) = image1(:,:,1) - bg(:,:,1) > -0.16;
+image1(:,:,1) = image1(:,:,1) - bg(:,:,1) < -0.16;
 image1(:,:,3) = image1(:,:,3) - bg(:,:,3) > -0.02;
 shadow = image1(:,:,3) - bg(:,:,3) < -0.27;
 image1(:,:,3) = image1(:,:,3) + shadow;
 
-image(:,:,1) = bwmorph(image(:,:,1),'erode',2);
+image1(:,:,1) = bwmorph(image1(:,:,1),'clean',2);
+image1(:,:,2) = bwmorph(image1(:,:,2),'clean',2);
+image1(:,:,3) = bwmorph(image1(:,:,3),'clean',2);
 
-imagesc(image1(:,:,1))
+figure(2)
+imagesc(image1(:,:,1));
+colormap(gray)
+
+for i = 1:3
+       [channel,num] = bwlabel(image1(:,:,i),4);
+       num;
+       stats = regionprops(channel, 'Area','PixelIdxList');
+       for m = 1:length(stats)
+            list(m) = stats(m).Area;
+       end
+
+       maxArea = max(list);
+       if i == 1
+          redArea = maxArea; 
+       elseif i == 2
+          greenArea = maxArea; 
+       elseif i == 3
+           blueArea = maxArea;
+       end
+end
+
+figure(3)
+imagesc(redArea)
 colormap(gray)
 
 %blue = image(:,:,2);
